0548063f12e8202da4e46c7a2357a3f4e50ca0e0
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-12-10 23:48:18 +0100

    feat (provider): configure sanctum or api auth middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        Sanctum::getAccessTokenFromRequestUsing(function (Request $request) {
+            // Attempt to retrieve the token from the 'X-Api-Key' header
+            $apiKey = $request->header('X-Api-Key');
+
+            // bearer token
+            $token = $request->bearerToken();
+
+            info('Full Request Headers: ' . json_encode($request->headers->all()));
+            info('API Key from X-Api-Key header: ' . $apiKey);
+            info('Bearer Token: ' . $token);
+
+            return $apiKey ?? $token;
+        });
     }
 }
